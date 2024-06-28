c547e21042b64fd19f3928e24241613fcb072b54
Zpeaxirious <151672708+Zpeaxirious@users.noreply.github.com>
2024-06-28 21:12:41 +0200

    smoll updates

--- script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('homeLink');
-    var updatedHref = link.getAttribute('href').replace('.html', '');
-    link.setAttribute('href', updatedHref);
-});
