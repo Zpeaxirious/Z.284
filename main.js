5618f1c90b73fd3a447eba1a292b30a778c73130
zpeaxirious <zpeaxirious@protonmail.com>
2024-10-20 16:33:00 +0200

    404

--- main.js
+++ main.js
@@ -23,8 +23,8 @@ document.addEventListener('visibilitychange', function() {
 
 /* Check for supported paths */
 window.addEventListener('load', function() {
-    const knownPaths = ['/', '/about', '/contact', '/gallery', '/index', '/lore', '/music', '/privacy-policy', '/stream', '/terms-of-service', '/unsupported', '/vtuber'];
-    if (!knownPaths.includes(window.location.pathname)) {
+    const knownPaths = ['/', '/about', '/contact', '/gallery', '/index', '/lore', '/music', '/privacy-policy', '/stream', '/terms-of-service', '/unsupported', '/vtuber', '/404'];
+    if (!knownPaths.includes(window.location.pathname) && window.location.pathname !== '/404') {
         window.location.href = '/404';
     }
 });
