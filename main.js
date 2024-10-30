1cc7535868f69dc68740ad98949f10131b4dbca2
zpeaxirious <zpeaxirious@protonmail.com>
2024-10-30 07:12:13 +0100

    Time A4

--- main.js
+++ main.js
@@ -74,15 +74,15 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-/* <span id="time" style="color: gray;"></span> */
+/* <div id="time"></div> */
 function updateTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
-    const timeString = `${hours}:${minutes}:${seconds}`;
-    document.getElementById('time').textContent = timeString;
+    const date = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const time = date.toLocaleTimeString();
+    const day = date.toLocaleDateString(undefined, options);
+    document.getElementById('time').innerHTML =
+        `Today is: ${day}, Current Time: ${time}`;
 }
 
-setInterval(updateTime, 1000); // Update time every second
-updateTime(); // Initial call to display time immediately
+setInterval(updateTime, 1000);
+updateTime();
